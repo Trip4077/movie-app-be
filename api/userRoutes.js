cba038ad1ccc9ef8b78d4c256cc5d8c035d12e71
Trip4077 <bj2336@email.vccs.edu>
2019-03-19 23:26:26 -0400

    PUT user complete

--- api/userRoutes.js
+++ api/userRoutes.js
@@ -10,7 +10,7 @@ const db = knex(knexConfig.development);
 router.post('/', (req, res) => {
     const user = req.body;
 
-    if(user.fullname && user.password && user.username) {
+    if(user.fullname && user.password && user.username && user.age) {
         db.insert(user)
         .into('users')
         .then(id => {
@@ -20,7 +20,7 @@ router.post('/', (req, res) => {
             res.status(500).json({ error: err, message: 'User Could Not Be Added'})
         })
     } else {
-        res.status(400).json({ errorMessage: "Please provide text for the post." })
+        res.status(400).json({ message: "Please provide text for the post." })
     }
 })
 
@@ -36,11 +36,32 @@ router.get('/:id', (req, res) => {
             }
         })
         .catch(err => {
-            res.status(500).json({ error: "The user information could not be retrieved." });
+            res.status(500).json({ error: err, message: "The user information could not be retrieved." });
         })
 })
 
 //EDIT USER INFO
+router.put('/:id', (req, res) => {
+    const changes = req.body;
+    const { id } = req.params;
 
+    if(changes.fullname || changes.username || changes.password || changes.age) {
+        db('users')
+        .where({ id })
+        .update(changes)
+        .then(success => {
+            if(success) {
+                res.status(201).json(changes);
+            } else {
+              res.status(404).json({ message: "The user with the specified ID does not exist." }) 
+            }
+        })
+        .catch(err => {
+          res.status(500).json({ error: err, message: "The user information could not be retrieved." });
+        })
+    } else {
+        res.status(500).json({ message: "Please enter a field to update"})
+    }
+})
 
 module.exports = router;
